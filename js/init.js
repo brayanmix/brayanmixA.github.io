b0287759ebdb83b384c436f0fc44111d308babdc
brayanmix <81206587+brayanmix@users.noreply.github.com>
2021-03-23 07:59:02 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,12 +1,15 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
-firebase.initializeApp({
-    apiKey: "AIzaSyBKXKRaQCWdfiSCZR39wunK0QApZMhgeqE",
-    authDomain: "pauten43.firebaseapp.com",
-    projectId: "pauten43",
-    storageBucket: "pauten43.appspot.com",
-    messagingSenderId: "167024006136",
-    appId: "1:167024006136:web:2766577637b6ab37c43c0d",
-    measurementId: "G-S4KD63PHHW"
-    });
+  var firebaseConfig = {
+    apiKey: "AIzaSyCPSDM5eYDNZJgXOESLhT4xWHCJJTdFuXU",
+    authDomain: "autentificamix.firebaseapp.com",
+    projectId: "autentificamix",
+    storageBucket: "autentificamix.appspot.com",
+    messagingSenderId: "164779494768",
+    appId: "1:164779494768:web:a20abf55dc21c08e7b53b5",
+    measurementId: "G-E9BVY37MZS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
